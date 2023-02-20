16f0a9c43129cb6c2498c682eff3c51c6d477164
jagsii <jagsii@gmail.com>
2023-02-21 00:41:04 +0800

    .

---
+++ book/ahdsez.com/content.min.js
@@ -0,0 +1,1 @@
+exports=async function(a,b,c,d){const{parseContent:e}=await require("transcode/parse_cctxt_content");return e(a,b,c,{clientOptions:{...(d&&d.clientOptions),encoding:"gbk"}})};
---
+++ book/koufood.com/content.min.js
@@ -0,0 +1,1 @@
+exports=async function(a,b,c,d){const{parseContent:e}=await require("transcode/parse_content");return e(a,"div#content",{clientOptions:{...(d&&d.clientOptions)}})};
